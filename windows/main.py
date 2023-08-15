a221779a72a16b77e3481091db11a9cf0919bb99
FangZirui-E5 <1275589811@qq.com>
2023-08-15 13:52:59 +0800

    [Add]🎊Windows Edition Turn on the maximum volume

--- windows/main.py
+++ windows/main.py
@@ -4,11 +4,15 @@ import subprocess
 import tkinter as tk
 from tkinter import messagebox
 import threading
-import screeninfo  # 导入获取屏幕分辨率的库
+import screeninfo
+import time
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 def play_video(filepath, resolution):
     clip = VideoFileClip(filepath)
-    clip = clip.resize(resolution)  # 将视频分辨率设置为屏幕分辨率
+    clip = clip.resize(resolution)
     clip.preview(fullscreen=True)
     clip.reader.close()
     clip.audio.reader.close_proc()
@@ -32,20 +36,46 @@ def unlock_keyboard():
     keyboard.unblock_key('esc')
 
 def show_message_box():
-    messagebox.showinfo("提示", "观影感受如何？🤣")
+    messagebox.showinfo("问候", "观影体验如何？🤣")
 
 def get_screen_resolution():
     screen = screeninfo.get_monitors()[0]
     return screen.width, screen.height
 
+def set_volume(volume_level):
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volume.SetMasterVolumeLevelScalar(volume_level, None)
+
+def control_volume(max_volume):
+    interval = 0.01
+    while True:
+        set_volume(max_volume)
+        time.sleep(interval)
+
+def toggle_mute():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volume.SetMute(0, None)
+
 def main():
     filepath = "source/video.mp4"
     screen_resolution = get_screen_resolution()
+    max_volume = 1.0
 
     lock_keyboard()
 
     play_thread = threading.Thread(target=play_video, args=(filepath, screen_resolution))
+    volume_thread = threading.Thread(target=control_volume, args=(max_volume,))
+    mute_thread = threading.Thread(target=toggle_mute)
+
     play_thread.start()
+    volume_thread.start()
+    mute_thread.start()
 
     play_thread.join()
 
@@ -55,4 +85,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
